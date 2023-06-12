7d51a0f8dbe27354edc9dbabd24ba241acaba533
jfeching <jeching@up.edu.ph>
2023-06-12 08:49:50 +0800

    fix: syntax

--- test.js
+++ test.js
@@ -269,12 +269,12 @@ async function main() {
   }
 
   //listens to keyboard events
-  sliders = document.getElementById("sliders");
+  let sliders = document.getElementById("sliders");
   var isTopView = false;
   document.addEventListener('keydown', (event) => {
     //Press T to move the camera position to "top view"
     if (event.key == 'T' || event.key == "t") {
-      if(!isTopView){
+      if (!isTopView) {
         cameraPosition[1] = 10;
         isTopView = true;
       } else {
@@ -293,7 +293,7 @@ async function main() {
     } else if (event.key == 'R' || event.key == "r") {
       cameraPosition[1] = 0;
     } else if (event.key == 'O' || event.key == 'o') {
-      if (sliders.classList.contains("invisible")){
+      if (sliders.classList.contains("invisible")) {
         sliders.classList.remove("invisible");
       } else sliders.classList.add("invisible");
     } else if (event.key == ' ') {
